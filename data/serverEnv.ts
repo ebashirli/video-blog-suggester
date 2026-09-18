45662ca832894967a992eb7ffb59971a6d44149d
Elvin Bashirli <ebashirli@gmail.com>
2026-09-18 11:45:31 +0200

    Setup Drizzle

---
+++ data/serverEnv.ts
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const serverEnv = createEnv({
+    server: {
+        DATABASE_URL: z.url(),
+    },
+
+    experimental__runtimeEnv: process.env,
+    emptyStringAsUndefined: true,
+    
+    // experimental__runtimeEnv: {
+    //     NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    // }
+})
